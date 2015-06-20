a0063665134274abc8121764572733a190788aee
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-06-20 03:34:36 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ab-stage
+
+Flash-like handling of viewport in HTML5.
+
+## Install
+
+With [npm](http://npmjs.org) do:
+
+```bash
+$ npm install ab-stage --save-dev
+```
+
+## Usage
+
+	require('ab-element');
+
+	// good 2 go
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function() {};
+
+window.stage = {};
+
+window.stage.width = function(use_px) {
+	var w=window,
+		d=document,
+		e=d.documentElement,
+		g=d.getElementsByTagName('body')[0],
+		x=w.innerWidth||e.clientWidth||g.clientWidth,
+		return (use_px ? x + 'px' : x);
+}
+window.stage.height = function(use_px) {
+	var w=window,
+		d=document,
+		e=d.documentElement,
+		g=d.getElementsByTagName('body')[0],
+		y=w.innerHeight||e.clientHeight||g.clientHeight;
+		return (use_px ? y + 'px' : y);
+}
