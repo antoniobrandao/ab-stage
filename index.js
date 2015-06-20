26a224a041aabb95437e5f368e354c327f3bde57
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-06-20 03:54:10 +0100

    Added window resize listener creation

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ window.stage.height = function(use_px) {
 		return (use_px ? y + 'px' : y);
 }
 
+
+window.stage.onResize = function( callBack )
+{
+	if(window.attachEvent) 			 	{window.attachEvent('onresize', callBack);
+	} else if(window.addEventListener) 	{window.addEventListener('resize', callBack, true);
+	} else { 							//The browser does not support Javascript event binding
+	}
+}
+
+window.stage.removeOnResize = function(callBack)
+{
+	if(window.detachEvent)  				{ window.detachEvent('onresize', 		callBack);
+	} else if(window.removeEventListener) 	{ window.removeEventListener('resize', 	callBack);
+	} else { 								//The browser does not support Javascript event binding
+	}
+}
+
 // filters
 // mouseEnabled (interactivity blocker)
 // add / remove child
